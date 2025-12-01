44094b7c551ec381774af8b485785b79a0248870
Shahi1810 <shahi.divyansh18@gmail.com>
2025-12-02 00:22:43 +0530

    sliding winndow and 2 pointer first question

---
+++ Sliding_window_2_pointer/src/cards.java
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
+public class cards {
+    public int maxScore(int[] cardPoints, int k) {
+        int lsum=0, rsum=0, maxSum=0;
+        for(int i=0; i<k;i++){
+            lsum=lsum+cardPoints[i];
+        }
+        maxSum=lsum;
+        int rindex=cardPoints.length-1;
+        for(int i=k-1;i>=0;i--){
+            lsum=lsum-cardPoints[i];
+            rsum=rsum+cardPoints[rindex];
+            rindex--;
+            maxSum=Math.max(maxSum,(lsum+rsum));
+        }
+        return maxSum;
+    }
+}
